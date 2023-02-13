dc9c1930556faed099c6cdfad3198c61becf2313
crazyoptimist <hey@crazyoptimist.net>
2023-02-13 13:48:41 -0600

    test: make it fail

--- voa_downloader_test.go
+++ voa_downloader_test.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
 
 func TestGetMp3List(t *testing.T) {
-	// buf, err := ioutil.ReadFile("testdata/rss_sample.txt")
-	// if err != nil {
-	// 	t.Errorf("rss sample file read failed")
-	// }
+	buf, err := ioutil.ReadFile("testdata/rss_sample.txt")
+	if err != nil {
+		t.Errorf("rss sample file read failed")
+	}
 
-	got := GetMp3List()
-	want := []string{""}
+	got := GetMp3List(string(buf), 2)
+	want := []string{"https://av.voanews.com/clips/VEN/2022/04/21/20220421-220500-VEN060-program.mp3", "https://av.voanews.com/clips/VEN/2022/04/20/20220420-220500-VEN060-program.mp3"}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
